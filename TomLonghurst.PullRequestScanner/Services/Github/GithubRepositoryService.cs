c2d147a5ba662eafc8bf1eb692ecab827972ced1
Tom Longhurst <thomhurst@gmail.com>
2022-07-04 14:13:41 +0100

    Fix

--- TomLonghurst.PullRequestScanner/Services/Github/GithubRepositoryService.cs
+++ TomLonghurst.PullRequestScanner/Services/Github/GithubRepositoryService.cs
@@ -6,16 +6,17 @@ namespace TomLonghurst.PullRequestScanner.Services.Github;
 
 internal class GithubRepositoryService : BaseGitHubApiService, IGithubRepositoryService
 {
+    private readonly PullRequestScannerOptions _pullRequestScannerOptions;
     private readonly GithubOptions _githubOptions;
 
-    public GithubRepositoryService(GithubHttpClient githubHttpClient, GithubOptions githubOptions) : base(githubHttpClient)
+    public GithubRepositoryService(GithubHttpClient githubHttpClient, PullRequestScannerOptions pullRequestScannerOptions) : base(githubHttpClient)
     {
-        _githubOptions = githubOptions;
+        _pullRequestScannerOptions = pullRequestScannerOptions;
     }
 
     public async Task<List<GithubRepository>> GetGitRepositories()
     {
-        var gitRepositoryResponse = await Get<GithubRepository>(_githubOptions.CreateUriPathPrefix() + "repos");
+        var gitRepositoryResponse = await Get<GithubRepository>(_pullRequestScannerOptions.Github.CreateUriPathPrefix() + "repos");
 
         return gitRepositoryResponse
             .Where(x => !x.Disabled)
